643dd1374640087df09f4e8b2ad19ea849299aef
大溝桂 <kay@komizo-mac.local>
2015-05-31 02:43:47 +0900

    first commit

---
+++ src/main/webapp/mqttApp.js
@@ -0,0 +1,59 @@
+/**
+ * MQTT Web Client Sample
+ * This sample message using mqtt over http
+ */
+angular.module('mqttApp', []).controller('mqttController', function($scope) {
+	var mqttCli = this;
+	var client;
+
+	$scope.topicMessages = [ {
+		text : ""
+	} ];
+	$scope.servers = [ {
+		text: "ws://test.mosquitto.org:8080/mqtt"
+	}, {
+		text: "ws://localhost:8080/mqtt"
+	} ];
+
+	$scope.mqttServerSelected = "";
+	$scope.mqttServerAddr = $scope.servers[0].text;
+	this.connectionStatus = "DISCON";
+	this.subscribeTopicName = "redhatkkdemo";
+	this.publishTopicName = "redhatkkdemo";
+	this.publishMessage = "Hello World!";
+	
+	
+	// ------------------------------
+	// FUNCTIONS
+	
+	// update server input field with selected server
+	$scope.updateServer = function() {
+		$scope.mqttServerAddr = $scope.mqttServerSelected.text;
+	}
+
+	mqttCli.connect = function() {
+		client = mqtt.connect($scope.mqttServerAddr);
+		connectionStatus = "CONNECTED";
+	};
+
+	mqttCli.subscribe = function() {
+		client.subscribe(this.subscribeTopicName);
+		client.on("message", function(topic, payload) {
+			var msg = [ topic, payload ].join(": ");
+			//this.topicMessage = msg;
+			// client.end();
+			if ($scope.topicMessages.length > 5) {
+				$scope.topicMessages.shift();
+			}
+			;
+			$scope.topicMessages.push({
+				text : msg
+			});
+		});
+	};
+
+	mqttCli.publish = function() {
+		client.publish(this.publishTopicName, this.publishMessage);
+	};
+
+});
